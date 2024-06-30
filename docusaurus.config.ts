e8281b6f671962bc99e62f4c99601e054e5e8d1a
Yuze075 <91772256+Yuze075@users.noreply.github.com>
2024-06-30 17:43:12 +0800

    更新Action Invoker的文档说明

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -3,99 +3,99 @@ import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
 const config: Config = {
-  title: 'YuzeToolkit',
-  tagline: '一个游戏开发工具包',
-  favicon: 'img/favicon.ico',
+    title: 'YuzeToolkit',
+    tagline: '一个游戏开发工具包',
+    favicon: 'img/favicon.ico',
 
-  // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+    // Set the production url of your site here
+    url: 'https://your-docusaurus-site.example.com',
+    // Set the /<baseUrl>/ pathname under which your site is served
+    // For GitHub pages deployment, it is often '/<projectName>/'
+    baseUrl: '/',
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'Yuze075', // Usually your GitHub org/user name.
-  projectName: 'YuzeToolkit', // Usually your repo name.
+    // GitHub pages deployment config.
+    // If you aren't using GitHub pages, you don't need these.
+    organizationName: 'Yuze075', // Usually your GitHub org/user name.
+    projectName: 'YuzeToolkit', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+    onBrokenLinks: 'throw',
+    onBrokenMarkdownLinks: 'warn',
 
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
-  i18n: {
-    defaultLocale: 'zh-Hans',
-    locales: ['zh-Hans'],
-  },
+    // Even if you don't use internationalization, you can use this field to set
+    // useful metadata like html lang. For example, if your site is Chinese, you
+    // may want to replace "en" with "zh-Hans".
+    i18n: {
+        defaultLocale: 'zh-Hans',
+        locales: ['zh-Hans'],
+    },
 
-  presets: [
-    [
-      'classic',
-      {
-        docs: {
-          sidebarPath: './sidebars.ts',
-        },
-        blog: {
-          showReadingTime: true,
-        },
-        theme: {
-          customCss: './src/css/custom.css',
-        },
-      } satisfies Preset.Options,
+    presets: [
+        [
+            'classic',
+            {
+                docs: {
+                    sidebarPath: './sidebars.ts',
+                },
+                blog: {
+                    showReadingTime: true,
+                },
+                theme: {
+                    customCss: './src/css/custom.css',
+                },
+            } satisfies Preset.Options,
+        ],
     ],
-  ],
 
-  markdown: {
-    format: 'detect',
-  },
+    markdown: {
+        format: 'detect',
+    },
 
-  themeConfig: {
-    // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
-    navbar: {
-      title: 'YuzeToolkit',
-      logo: {
-        alt: 'YuzeToolkit Logo',
-        src: 'img/logo.svg',
-      },
-      items: [
-        {
-          type: 'docSidebar',
-          sidebarId: 'tutorialSidebar',
-          position: 'left',
-          label: 'Tutorial',
+    themeConfig: {
+        // Replace with your project's social card
+        image: 'img/docusaurus-social-card.jpg',
+        navbar: {
+            title: 'YuzeToolkit',
+            logo: {
+                alt: 'YuzeToolkit Logo',
+                src: 'img/logo.svg',
+            },
+            items: [
+                {
+                    type: 'docSidebar',
+                    sidebarId: 'tutorialSidebar',
+                    position: 'left',
+                    label: 'Tutorial',
+                },
+                {
+                    to: '/blog',
+                    label: 'Blog',
+                    position: 'left'
+                },
+                {
+                    type: 'docsVersionDropdown',
+                    position: 'right',
+                },
+                {
+                    type: 'localeDropdown',
+                    position: 'right',
+                },
+                {
+                    href: 'https://github.com/Yuze075/YuzeToolkit',
+                    label: 'GitHub',
+                    position: 'right',
+                },
+            ],
         },
-        {
-          to: '/blog', 
-          label: 'Blog', 
-          position: 'left'
+        footer: {
+            style: 'dark',
+            links: [],
+            copyright: `Copyright Yuze © ${new Date().getFullYear()} 赣ICP备2023003651号`,
         },
-        {
-          type: 'docsVersionDropdown',
-          position: 'right',
+        prism: {
+            theme: prismThemes.github,
+            darkTheme: prismThemes.dracula,
         },
-        {
-          type: 'localeDropdown',
-          position: 'right',
-        },
-        {
-          href: 'https://github.com/Yuze075/YuzeToolkit',
-          label: 'GitHub',
-          position: 'right',
-        },
-      ],
-    },
-    footer: {
-      style: 'dark',
-      links: [      ],
-      copyright: `Copyright Yuze © ${new Date().getFullYear()} 赣ICP备2023003651号`,
-    },
-    prism: {
-      theme: prismThemes.github,
-      darkTheme: prismThemes.dracula,
-    },
-  } satisfies Preset.ThemeConfig,
+    } satisfies Preset.ThemeConfig,
 };
 
 export default config;
